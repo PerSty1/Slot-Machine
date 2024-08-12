7f18422a0640e799bb0c42bf4dfaff3aa8443d6f
PerSty1 <svjatik78@gmail.com>
2024-08-12 19:27:51 +0300

    chore: Rename CSS file to style.css and update image sources in HTML file

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('spinButton').addEventListener('click', function() {
-    const items = document.querySelectorAll('.slot__item');
-    items.forEach(item => {
-        let randomIndex = Math.floor(Math.random() * 5); // 5 можливих позицій для прокруту
-        let translateY = -100 * randomIndex; // Розмір зображення
-        item.style.transform = `translateY(${translateY}px)`;
-    });
-});
